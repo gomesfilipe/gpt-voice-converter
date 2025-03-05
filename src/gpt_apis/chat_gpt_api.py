3d1c6b870e10fe829f47d5e10fadf7652a2dfe62
Christian Junji <junjao.ccomp@gmail.com>
2025-03-05 20:37:10 -0300

    feat: communicating with gemini api

--- src/gpt_apis/chat_gpt_api.py
+++ src/gpt_apis/chat_gpt_api.py
@@ -1,5 +1,34 @@
 from src.gpt_apis.gpt_api import GptApi
+from dotenv import load_dotenv
+from google import genai
+
+import os
+
+# Diretório raiz do projeto
+root_path = os.getcwd()
+env_path = os.path.join(root_path, ".env")
+
+load_dotenv(dotenv_path=env_path)
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+if api_key is None:
+    raise ValueError("GEMINI_API_KEY não configurada")
+
+client = genai.Client(api_key=api_key)
 
 class ChatGptApi(GptApi):
-  def send(self, text: str):
-    return 'MENSAGEM CHAT GPT'
+  def send(self, text: str) -> str:
+        try:
+            if not hasattr(self, "prompt"):
+                self.prompt = "Responda como uma conversa entre humanos, não utilize emojis: "
+            self.prompt += text
+
+            response = client.models.generate_content(
+                model="gemini-2.0-flash",
+                contents=self.prompt
+            )
+
+            return response.text
+        except Exception as e:
+            return f"Erro na chamada da API: {str(e)}"
